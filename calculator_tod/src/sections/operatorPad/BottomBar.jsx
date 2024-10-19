7d125ec83aa0b4163c9652b06d3497fe9f24f5a2
Stephen <121040221+stephenstrange-blip@users.noreply.github.com>
2024-10-19 20:51:59 +0800

    refactor: Add a clear button

--- calculator_tod/src/sections/operatorPad/BottomBar.jsx
+++ calculator_tod/src/sections/operatorPad/BottomBar.jsx
@@ -6,6 +6,7 @@ function BottomBar () {
       <div className='numGrid zero'>0</div>
       <div className='numGrid addition operator'>+</div>
       <div className='numGrid subtraction operator'>-</div>
+      <div className='numGrid equate operator'>=</div>
     </div>
   )
 }
--- calculator_tod/src/sections/operatorPad/SideBar.jsx
+++ calculator_tod/src/sections/operatorPad/SideBar.jsx
@@ -3,9 +3,10 @@ import './sideBar.css'
 function SideBar () {
   return (
     <div className='side-bar'>
+      <div className='numGrid clear operator'>clr</div>
       <div className='numGrid multiplication operator'>*</div>
       <div className='numGrid division operator'>/</div>
-      <div className='numGrid equate operator'>=</div>
+      
     </div>
   )
 }
